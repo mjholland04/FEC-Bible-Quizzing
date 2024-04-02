8a77f71c3d5c659099a5e703d729109c5573aa21
Jada Bonnett <jadabonnett@gmail.com>
2024-04-02 14:05:39 -0400

    more minor changes, players are not working yet

--- graceproject/src/App.js
+++ graceproject/src/App.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function Player(n, ps, pf, ca){
-  this.name = n;
-  this.personalScore = ps;
-  this.personalFouls = pf;
-  this.correctAnswers = ca
+//TO-DO
+// - add in score keeping for each question, make that able to send to database after the end
+// - finish making the scores be attrubuted to the selected player
+// - 
+
+function Player(name, personalScore, personalFouls, correctAnswers){
+  this.name = name;
+  this.personalScore = personalScore;
+  this.personalFouls = personalFouls;
+  this.correctAnswers = correctAnswers
 }
 
 let player1 = new Player("Jada", 100, 0, 5);
@@ -16,40 +21,43 @@ let player5 = new Player("DMike", 40, 3, 2);
 let player6 = new Player("Dyson", 40, 1, 2);
 let currentQuizzer = player1;
 
-function Team(s, t, f, p, n, l, c){
-  this.score = s;
-  this.timeouts = t;
-  this.fouls = f;
-  this.players = p;
-  this.teamName = n;
-  this.league = l;
-  this.church = c;
+function Team(score, timeouts, fouls, players, teamName, league, church){
+  this.score = score;
+  this.timeouts = timeouts;
+  this.fouls = fouls;
+  this.players = players;
+  this.teamName = teamName;
+  this.league = league;
+  this.church = church;
 }
 
 let teamA = new Team(180, 3,2,[player1, player2, player3, player4],"Upland 1","HS","UCC");
 let teamB = new Team(80,2,4,[player5, player6],"Upland 2","MS","Taylor Chapel");
 
-const TeamScoreboard = ({ teamName }) => {
+function setListA(players){
+  let dropdown = document.getElementById("TeamAPlayers");
+  var opt = document.createElement("option"); 
+  opt.text = players.name;
+  opt.value = players.id; //values have to be strings... not sure what to do about that yet trying to get the correct player when chosen to attribute points
+  //may have to wait to make those changes until we are not using a dropdown.
+  dropdown.options.add(opt);
+}
+
+function setListB(players){
+  let dropdown = document.getElementById("TeamBPlayers");
+  var opt = document.createElement("option"); 
+  opt.text = players.name;
+  opt.value= players; 
+  dropdown.options.add(opt);
+}
+
+const TeamScoreboard = () => {
   const [teamAScore, setTeamAScore] = useState(teamA.score);
   const [teamAFouls, setTeamAFoulCount] = useState(teamA.fouls);
+  const [teamATimeouts, setTeamATimeouts] = useState (teamA.timeouts)
   const [teamBScore, setTeamBScore] = useState(teamB.score);
-  const [teamBFouls, setTeamBFoulCount] = useState(teamA.fouls);
-
-  // const handleScoreChange = (value) => {
-  //   setTeamAScore(teamAScore + value);
-  // };
-
-  function detectFouls(team) {
-    if (team === teamA) {
-      teamA.fouls += 1;
-      setTeamAFoulCount(team.fouls)
-      // Additional logic for Team A fouls...
-    } else{
-      teamB.fouls += 1;
-      setTeamBFoulCount(team.fouls)
-      // Additional logic for Team B fouls...
-    }
-  }
+  const [teamBFouls, setTeamBFoulCount] = useState(teamB.fouls);
+  const [teamBTimeouts, setTeamBTimeouts] = useState (teamB.timeouts)
   
   // Function to handle foul button clicks
   function handleFoulButtonClick(team) {
@@ -88,6 +96,7 @@ const TeamScoreboard = ({ teamName }) => {
   
   // Function to handle incorrect answer button clicks
   function handleIncorrectButtonClick(team) { //is there a rule for if the team gets so many answers wrong?
+    //getCurrentPlayer(team)
     if (team === teamA) {
       team.score -= 10;
       currentQuizzer.personalScore -= 10;
@@ -122,49 +131,63 @@ const TeamScoreboard = ({ teamName }) => {
   
   // Function to handle timeout button clicks
   function handleTimeoutButtonClick(team) {
-    team.timeouts += 1;
-    // if (team === teamA){
-    //   document.getElementById(
-    //     "teamATimeouts"
-    //   ).textContent = `Timeouts for Team A: ${QuizMatch.teamATimeouts}`;
-    // }else{
-    //   document.getElementById(
-    //     "teamBTimeouts"
-    //   ).textContent = `Timeouts for Team B: ${QuizMatch.teamBTimeouts}`;
-    // }
+    if (team === teamA){
+      if (teamA.timeouts >= 3){
+        alert("No Timeouts Left")
+      }else{
+        teamA.timeouts += 1
+        setTeamATimeouts(teamA.timeouts)
+      }
+    }else{
+      if(teamB.timeouts >= 3){
+        alert("No Timeouts Left")
+      }else{
+        team.timeouts += 1;
+        setTeamBTimeouts(teamB.timeouts)
+      }
+    }
   }
-  // const handleFoul = () => {
-  //   setFoulCount(foulCount + 1);
-  // };
+
+  function getCurrentPlayer(team){
+    if (team === teamA){
+      var selector = document.getElementById("TeamAPlayers")
+      currentQuizzer = selector.value
+      console.log(currentQuizzer)
+    }else{
+      var selector = document.getElementById("TeamBPlayers")
+      currentQuizzer = selector.value
+    }
+  }
 
   return (
     <div>
       <h2>{teamA.teamName} Score: {teamAScore}</h2>
+      <select onClick={() => teamA.players.forEach(setListA)} id= "TeamAPlayers"></select>
       <button onClick={() => handleBonusButtonClick(teamA)}>+10</button>
       <button onClick={() => handleIncorrectButtonClick(teamA)}>-10</button>
       <button onClick={() => handleCorrectButtonClick(teamA)}>+20</button>
       <button onClick={() => handleNeg20(teamA)}>-20</button>
       <button onClick={() => handleFoulButtonClick(teamA)}>Foul</button>
-      <p>Foul Count: {teamAFouls}</p>
+      <button onClick={() => handleTimeoutButtonClick(teamA)}>Timeout</button>
+      <p>Foul Count: {teamAFouls}  Timeouts Taken: {teamATimeouts}</p>
       
       <h2>{teamB.teamName} Score: {teamBScore}</h2>
+      <select onClick={() => teamB.players.forEach(setListB)} id= "TeamBPlayers"></select>
       <button onClick={() => handleBonusButtonClick(teamB)}>+10</button>
       <button onClick={() => handleIncorrectButtonClick(teamB)}>-10</button>
       <button onClick={() => handleCorrectButtonClick(teamB)}>+20</button>
-      <button onClick={() => handleNeg20(teamA)}>-20</button>
+      <button onClick={() => handleNeg20(teamB)}>-20</button>
       <button onClick={() => handleFoulButtonClick(teamB)}>Foul</button>
-      <p>Foul Count: {teamBFouls}</p>
+      <button onClick={() => handleTimeoutButtonClick(teamB)}>Timeout</button>
+      <p>Foul Count: {teamBFouls}  Timeouts Taken: {teamBTimeouts}</p>
     </div>
-    
-    
   );
 };
 
 const App = () => {
   return (
     <div>
-      <TeamScoreboard teamName={teamA} />
-      {/* <TeamScoreboard teamName={teamB.teamName} /> */}
+      <TeamScoreboard/>
     </div>
   );
 };
