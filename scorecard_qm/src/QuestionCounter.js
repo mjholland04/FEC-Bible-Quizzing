dfbe22da30c9d4d3fdc96b67b8b2d1d4bc0f4565
Colten Yeats <coltenyeats@gmail.com>
2024-04-12 09:56:08 -0400

    score counter beta + css additions

--- scorecard_qm/src/QuestionCounter.js
+++ scorecard_qm/src/QuestionCounter.js
@@ -1,19 +1,42 @@
 import React, { useState } from 'react';
 
+import './QuestionCounter.css';
+
+function ScoreCounter () {
+    const [counter, setCounter] = useState(0);
+    return (
+        <div className="counter-container">
+            <h3>Team 1 Score</h3>
+            <h1>{counter}</h1>
+            <div className="button-container">
+                <button onClick={() => { 
+                        setCounter(counter - 10)}
+                }>-</button>
+                <button onClick={() => {
+                    setCounter(counter + 10)}
+                }>+</button>
+            </div>
+        </div>
+    );
+}
+
 function QuestionCounter () {
     const [counter, setCounter] = useState(0);
     return (
         <div className="counter-container">
             <h3>Question Number</h3>
             <h1>{counter}</h1>
-            <button onClick={() => { 
-                if (counter > 0)
-                    setCounter(counter - 1)}
-            }>-</button>
-            <button onClick={() => {
-                if (counter < 25)
-                setCounter(counter + 1)}
-            }>+</button>
+            <div className="button-container">
+                <button onClick={() => { 
+                    if (counter > 0)
+                        setCounter(counter - 1)}
+                }>-</button>
+                <button onClick={() => {
+                    if (counter < 25)
+                    setCounter(counter + 1)}
+                }>+</button>
+            </div>
+            <ScoreCounter />
         </div>
 
     );
